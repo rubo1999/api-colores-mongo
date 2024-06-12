34f01a3aa219de114bb1f50ecad2d157f860cff8
Ruben <rubo.lopez.1999@alumnos.cei.es>
2024-06-12 09:16:34 +0200

    api con mongo terminada

---
+++ db.js
@@ -0,0 +1,73 @@
+require("dotenv").config();
+const {MongoClient,ObjectId} = require("mongodb")
+
+
+function conectar(){
+    return MongoClient.connect(process.env.URL_MONGO);
+}
+
+function leerColores(){
+    return new Promise(async (ok,ko) => {
+        
+        try{
+            const conexion = await conectar();
+
+            let colores = await conexion.db("colores").collection("colores").find({}).toArray();
+
+            conexion.close();
+
+            ok(colores.map(({_id,r,g,b}) => {
+                return {id:_id,r,g,b}
+            }));
+
+        }catch(error){
+            ko({error : "Error en la base de datos"});
+        }
+    })
+}
+
+
+function crearColor({r,g,b}){
+    return new Promise(async (ok,ko) => {
+    
+        try{
+            
+            const conexion = await conectar();
+
+            let {insertedId} = await conexion.db("colores").collection("colores").insertOne({r,g,b})
+
+            conexion.close();
+
+            ok(insertedId);
+
+        }catch(error){
+            ko({error : "Error en la base de datos"});
+        }
+    })
+}
+//'666946982e6703ce3d2b0e50'
+
+function borrarColor({id}){
+    return new Promise(async (ok,ko) => {
+        
+        try{
+
+            const conexion = await conectar();
+
+            let {deletedCount} = await conexion.db("colores").collection("colores").deleteOne({ _id : new ObjectId(id)})
+
+            conexion.close();
+
+            ok(deletedCount);
+        }
+        catch(error){
+            ko({error : "Error en la base de datos"});
+        }
+    })
+}
+
+
+module.exports = {leerColores, crearColor, borrarColor};
+
+
+
---
+++ index.js
@@ -0,0 +1,63 @@
+require("dotenv").config();
+const express = require("express");
+const {json} = require("body-parser");
+const cors = require("cors");
+
+const {leerColores,crearColor, borrarColor} = require("./db");
+
+const servidor = express();
+
+servidor.use(cors());
+
+servidor.use(json());
+
+if(process.env.PRUEBAS){
+    servidor.use(express.static("./pruebas"));
+}
+
+servidor.get("/colores", async (peticion,respuesta) => {
+    try{
+        let colores = await leerColores();
+
+        respuesta.json(colores);
+
+    }catch(error){
+        respuesta.status(500);
+
+        respuesta.json(error);
+    }
+})
+
+servidor.post("/nuevo", async (peticion,respuesta) => {
+    try{
+        let id = await crearColor(peticion.body);
+
+        respuesta.json({id});
+
+    }catch(error){
+        respuesta.status(500);
+
+        respuesta.json(error);
+    }
+})
+
+servidor.delete("/borrar", async (peticion, respuesta) => {
+    try{
+        let cantidad = await borrarColor(peticion.body);
+
+        respuesta.json({resultado : cantidad ? "ok" : "ko"});
+    }
+    catch(error){
+        respuesta.status(500);
+
+        respuesta.json(error);
+    }
+})
+
+servidor.use((peticion,respuesta) => {
+    respuesta.status(404);
+    respuesta.json({error: "not found"});
+})
+
+
+servidor.listen(process.env.PORT);
